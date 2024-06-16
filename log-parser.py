9ca10fe2943f17934416c240bc8e7f5faace371b
enisik <51776245+enisik@users.noreply.github.com>
2024-06-16 21:59:16 +0200

    added log-parser: converting logfiles to list of event-dicts

---
+++ log-parser.py
@@ -0,0 +1,55 @@
+
+
+def get_events_from(path):
+    # 
+    # https://stackoverflow.com/questions/30627810/how-to-parse-this-custom-log-file-in-python
+    #
+    import re
+
+    start = re.compile(r'\[[0-9a-f]+\] \{')
+    end = re.compile(r'\[[0-9a-f]+\] [a-z-]+}')
+
+    def generateDicts(log_fh):
+        currentDict = {}
+        for line in log_fh:
+            # ignore gc-minor-walkroots entries
+            if "gc-minor-walkroots" in line: 
+                continue
+
+            if start.match(line):
+                splitted_line = line.split("{")
+                currentDict = {"start": splitted_line[0][1:-2], "task": splitted_line[1][:-1], "text": ""}
+            
+            elif end.match(line):
+                splitted_line = line.split("]")
+                currentDict["end"] = splitted_line[0][1:]
+                yield currentDict
+            
+            elif "time since end of last minor GC" in line: 
+                splitted_line = line.split(":")
+                currentDict["time-last-minor-gc"] = float(splitted_line[1])
+
+            elif "minor collect, total memory used" in line:
+                splitted_line = line.split(":")
+                currentDict["total-memory-used"] = int(splitted_line[1])
+
+            elif "time taken" in line:
+                splitted_line = line.split(":")
+                currentDict["time-taken"] = float(splitted_line[1])
+
+            elif "major collection threshold" in line:
+                splitted_line = line.split(":")
+                currentDict["new-threshold"] = float(splitted_line[1])
+
+            else:
+                currentDict["text"] += line
+
+
+    with open(path, "r") as f:
+        events = list(generateDicts(f))
+    return events
+
+if __name__ == "__main__":
+    events = get_events_from("logs/gcbench")
+    for item in events:
+        print(item)
