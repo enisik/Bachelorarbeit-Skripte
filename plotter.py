8a72f88c9d65e10a5ebbf1951193e741a4a31274
enisik <51776245+enisik@users.noreply.github.com>
2024-07-08 21:45:09 +0200

    subplots added

--- plotter.py
+++ plotter.py
@@ -84,25 +84,27 @@ def plot_gc(events, title="benchmark", fignum=0):
     t_threshold = np.linspace(0, time_memory[-1], len(time_memory)*50)
     y_threshold = heap_limit(t_threshold)
     
-    plt.figure(fignum, figsize=(10, 8))
+    fig = plt.figure(fignum, figsize=(15, 10))
     plt.clf()
-    plt.subplot(211)
+    fig.suptitle(title, fontsize=16)
+    ax = plt.subplot(311)
     for i in range(len(time_gc_start)):
-        plt.axvspan(
+        ax.axvspan(
             time_gc_start[i], time_gc_end[i], alpha=0.4, color='red')
 
     for i in range(len(time_major_gc_start)):
-        plt.axvspan(time_major_gc_start[i], time_major_gc_end[i], alpha=0.2, color='blue')
-    plt.plot(time_memory,memory, 'b-', label="heap usage at minor gc")
-    plt.plot(t_threshold, y_threshold, 'm', label="heap limit")
-    plt.grid()
-    ax = plt.gca()
+        ax.axvspan(time_major_gc_start[i], time_major_gc_end[i], alpha=0.2, color='blue')
+    ax.plot(time_memory,memory, 'b-', label="heap usage at minor gc")
+    ax.plot(t_threshold, y_threshold, 'r', label="heap limit")
+    ax.grid()
     ax.yaxis.set_major_formatter(EngFormatter("B"))
     ax.xaxis.set_major_formatter(EngFormatter("s"))
-    plt.ylabel("memory")
-    plt.title(title)
-    plt.legend()
-    plt.show()
+    ax.legend(loc='best',  ncol=2, #bbox_to_anchor=(0.058, 1.05),
+              fancybox=True)
+    #plt.xlabel("time")
+    #plt.ylabel("memory")
+    #plt.title(title) 
+    #plt.show()
 
 
 def plot_membalancer_heap_rule(events, mem_balancer, fignum):
@@ -111,11 +113,17 @@ def plot_membalancer_heap_rule(events, mem_balancer, fignum):
     nursery_size = events[4]["nursery-size"]
     mem_balancer.nursery_size = nursery_size
     total_memory_used_last_minor = 0
+    time_heartbeat = []
+    time_major = []
+    g_m_list, g_t_list = [], []
+    s_m_list, s_t_list = [], []
     for event in events[1:]:
         if event["task"] == "gc-minor":
+            time_heartbeat.append(event["time-start"])
             # total_memory_used = event["memory-before-collect"]
             total_memory_used = event["memory-after-collect"]
             g_m_next = total_memory_used - total_memory_used_last_minor
+            g_m_list.append(g_m_next)
             if g_m_next >= 0:
                 # memory allocated since last heartbeat
                 g_m = g_m_next
@@ -126,33 +134,58 @@ def plot_membalancer_heap_rule(events, mem_balancer, fignum):
                 #print(f"g_m_next: {g_m_next}")
             total_memory_used_last_minor = event["memory-after-collect"]
             g_t = event["time-last-minor-gc"]  # time since last heartbeat
+            g_t_list.append(g_t)
             mem_balancer.on_heartbeat(g_m, g_t)
             time_threshold.append(event["time-start"])
             heap_limit.append(mem_balancer.compute_M())
-            #print(f"heap_limit: {mem_balancer.heap_limit}, E: {mem_balancer.E}")
         elif event["task"] == "gc-collect-step":
             if "SCANNING" in event["text"]:
-                #print("major collect start")
                 time_taken = event["time-taken"]
             else:
                 time_taken += event["time-taken"]
         elif event["task"] == "gc-collect-done":
+            time_major.append(event["time-start"])
+            #time_heartbeat.append(time_major[-1])
             time_threshold.append(event["time-start"]+ event["time-taken"])
             bytes_collected = event["bytes-collected"]
+            s_m_list.append(bytes_collected)
             time_taken += event["time-taken"]
+            s_t_list.append(time_taken)
             live_memory = event["memory-after-collect"]
             mem_balancer.on_gc(bytes_collected, time_taken, live_memory)
+            #g_t = time_heartbeat[-2] - time_heartbeat[-1]
             # mem_balancer.on_heartbeat()
-            #print(f"s_m_smoothed: {mem_balancer.s_m_smoothed}, s_t_smoothed: {mem_balancer.s_t_smoothed}, g_m_smoothed: {
-            #      mem_balancer.g_m_smoothed}, g_t_smoothed: {mem_balancer.g_t_smoothed}")
             heap_limit.append(mem_balancer.compute_M())
-            #print(f"heap_limit: {mem_balancer.heap_limit}, E: {mem_balancer.E}")
-            #print("major collect done")
 
     t = np.linspace(0, events[-1]["time-start"], len(time_threshold)*50)[1:]
     index = np.searchsorted(time_threshold, t, 'right')
     y = np.array(heap_limit)[index-1]
     plt.figure(fignum)
-    plt.subplot(211)
-    plt.plot(t, y, 'k', label="MemBalancer heap rule")
-    plt.legend()
+    ax = plt.subplot(311)
+    ax.plot(t, y, 'k', label="MemBalancer heap rule")
+    ax.legend(loc='best', ncol=2, #bbox_to_anchor=(0.058, 1.05),
+              fancybox=True)
+    
+    ax = plt.subplot(312)
+    ax.grid()
+    ax.plot(time_heartbeat, g_m_list, label="$g_m$")
+    ax.plot(time_major, s_m_list, label="$s_m$")
+    ax.yaxis.set_major_formatter(EngFormatter("B"))
+    ax.xaxis.set_major_formatter(EngFormatter("s"))
+    ax.legend(loc='best', ncol=2,  # bbox_to_anchor=(0.058, 1.05),
+              fancybox=True)
+    #plt.ylabel("memory")
+    
+    ax = plt.subplot(313)
+    ax.grid()
+    g_m_list = np.array(g_m_list)
+    g_t_list = np.array(g_t_list)
+    s_m_list = np.array(s_m_list)
+    s_t_list = np.array(s_t_list)
+    ax.plot(time_heartbeat, g_m_list/g_t_list, label="$g_m / g_t$")
+    ax.plot(time_major, s_m_list/s_t_list, label="$s_m / s_t$")
+    ax.yaxis.set_major_formatter(EngFormatter("B/s"))
+    ax.xaxis.set_major_formatter(EngFormatter("s"))
+    ax.legend(loc='best', ncol=2,  # bbox_to_anchor=(0.058, 1.05),
+              fancybox=True)
+    #plt.ylabel("memory")
