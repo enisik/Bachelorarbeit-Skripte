686ffa8d28b5b813934047d909ac492e70096bbc
enisik <51776245+enisik@users.noreply.github.com>
2024-07-24 12:45:25 +0200

    Add argparser and hardcoded tuning factors

--- benchmark.py
+++ benchmark.py
@@ -1,18 +1,27 @@
-import subprocess
-import os
+import argparse
 import datetime
+import subprocess
 from tqdm import tqdm
+import os
 
 # PYPYLOG=gc:logs/bench-i ./pypy-c ~/Desktop/pypy/rpython/translator/goal/gcbench.py
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--it', type=int, required=True, help='Number of iterations', dest="iterations")
+    arguments = parser.parse_args()
     now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    dest_path = f"./logs/{now}"
     os_env = dict(os.environ)
-    os.makedirs(dest_path)
     file_path = os.path.expanduser(
         "~/Desktop/pypy/rpython/translator/goal/gcbench.py")
-    for i in tqdm(range(30), ncols=100):
-        subprocess.run(
-            ["./pypy-c", file_path], env=os_env | {"PYPYLOG": f"gc:{dest_path}/bench-{i}"}, capture_output=True)
+    program_path = os.path.expanduser(
+        "~/Desktop/pypy/pypy/goal/pypy-c"
+    )
+    for tuning_factor in [10, 100, 1_000, 10_000, 100_000]:
+        dest_path = f"./logs/{now}/{tuning_factor}"
+        os.makedirs(dest_path)
+        env = os_env | {"PYPYLOG": f"gc:{dest_path}/bench-%d", "PYPY_GC_MEMBALANCER_TUNING": str(tuning_factor)}
+        for i in tqdm(range(arguments.iterations), ncols=100):
+            subprocess.run(
+                [program_path, file_path], env=env, capture_output=True)
         
