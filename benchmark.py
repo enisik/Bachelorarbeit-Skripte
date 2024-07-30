c1d6bb8d5345f47fc576fb4a9daf6cb32f16ba73
enisik <51776245+enisik@users.noreply.github.com>
2024-07-30 16:55:20 +0200

    Add support for gc translate

--- benchmark.py
+++ benchmark.py
@@ -14,6 +14,8 @@ if __name__ == "__main__":
                         help='compiler/interpreter', dest="compiler_path")
     parser.add_argument('--t', type=str, required=True,
                         help='script to run', dest="file_path")
+    parser.add_argument('--source', type=str, required=False,
+                        help='source argument for gctranslate', dest="opt_source")
     parser.add_argument('--dest', type=str, required=True,
                         help='destination', dest="dest_path")
     arguments = parser.parse_args()
@@ -22,12 +24,16 @@ if __name__ == "__main__":
     file_path = os.path.expanduser(arguments.file_path)
     compiler_path = os.path.expanduser(arguments.compiler_path)
     dest_path_root = os.path.expanduser(arguments.dest_path)
+    args = [compiler_path, file_path]
+    if arguments.opt_source is not None:
+        args.append("--source")
+        args.append(arguments.opt_source)
     for tuning_factor in 10 ** np.linspace(2, 5, 20):
         print(
             f"===============\t\t tuning factor: {tuning_factor:<20} \t\t===============")
         dest_path = f"{dest_path_root}/{now}/{tuning_factor}"
         os.makedirs(dest_path)
-        env = os_env | {"PYPYLOG": f"gc:{dest_path}/bench-%d", "PYPY_GC_MEMBALANCER_TUNING": str(tuning_factor)}
+        env = os_env | {"PYPY_GC_MEMBALANCER_TUNING": str(tuning_factor)}
         for i in tqdm(range(arguments.iterations), ncols=100):
-            subprocess.run(
-                [compiler_path, file_path], env=env, capture_output=True)
+            env["PYPYLOG"] = f"gc:{dest_path}/{i}"
+            subprocess.run(args, env=env, capture_output=True)
