1808120e37a9819079cd83e0a73981d023d4b704
enisik <51776245+enisik@users.noreply.github.com>
2024-07-30 15:08:08 +0200

    Replace hardcoded benchmark arguments with commandline arguments.

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,6 @@
 import argparse
 import datetime
+import numpy as np
 import subprocess
 from tqdm import tqdm
 import os
@@ -9,19 +10,24 @@ import os
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--it', type=int, required=True, help='Number of iterations', dest="iterations")
+    parser.add_argument('--c', type=str, required=True,
+                        help='compiler/interpreter', dest="compiler_path")
+    parser.add_argument('--t', type=str, required=True,
+                        help='script to run', dest="file_path")
+    parser.add_argument('--dest', type=str, required=True,
+                        help='destination', dest="dest_path")
     arguments = parser.parse_args()
     now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     os_env = dict(os.environ)
-    file_path = os.path.expanduser(
-        "~/Desktop/pypy/rpython/translator/goal/gcbench.py")
-    program_path = os.path.expanduser(
-        "~/Desktop/pypy/pypy/goal/pypy-c"
-    )
-    for tuning_factor in [10, 100, 1_000, 10_000, 100_000]:
-        dest_path = f"./logs/{now}/{tuning_factor}"
+    file_path = os.path.expanduser(arguments.file_path)
+    compiler_path = os.path.expanduser(arguments.compiler_path)
+    dest_path_root = os.path.expanduser(arguments.dest_path)
+    for tuning_factor in 10 ** np.linspace(2, 5, 20):
+        print(
+            f"===============\t\t tuning factor: {tuning_factor} \t\t===============")
+        dest_path = f"{dest_path_root}/{now}/{tuning_factor}"
         os.makedirs(dest_path)
         env = os_env | {"PYPYLOG": f"gc:{dest_path}/bench-%d", "PYPY_GC_MEMBALANCER_TUNING": str(tuning_factor)}
         for i in tqdm(range(arguments.iterations), ncols=100):
             subprocess.run(
-                [program_path, file_path], env=env, capture_output=True)
-        
+                [compiler_path, file_path], env=env, capture_output=True)
